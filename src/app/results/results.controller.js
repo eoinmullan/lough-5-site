3f7abb238dcc00da7e5b6ab3991cf7e4f916fdca
eoinmullan <eoinmullan@hotmail.com>
2016-12-29 00:49:47 +0000

    Adding legacy results for 2009 - 2015.

---
+++ src/app/results/results.controller.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('lough5Site')
+    .controller('ResultsTabController', ResultsTabController);
+
+  /** @ngInject */
+  function ResultsTabController() {
+    var vm = this;
+    vm.tab = 8;
+
+    vm.setTab = function(newValue){
+      vm.tab = newValue;
+    };
+    vm.isSet = function(tabName){
+      return vm.tab === tabName;
+    };
+  }
+})();
